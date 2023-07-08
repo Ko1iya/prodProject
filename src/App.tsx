95719a8ebf9775089b8a6f7ec05972ec8fc866c4
Ko1iya <kolya.fialkovskiy@gmail.com>
2023-07-08 13:05:27 +0800

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src='/prod-project/build/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg'
+          className='App-logo'
+          alt='logo'
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
