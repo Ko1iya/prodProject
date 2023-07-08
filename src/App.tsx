24cc0732548fc3b64ee8b145a8e617842749cd1d
Ko1iya <kolya.fialkovskiy@gmail.com>
2023-07-08 13:10:37 +0800

    fix logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react"
-
+import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img
-          src='/prod-project/build/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg'
-          className='App-logo'
-          alt='logo'
-        />
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
